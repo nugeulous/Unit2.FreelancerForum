e97943d2e68234b302c56446cd11f771bcb6bc38
nugeulous <c.ryan.nugent@gmail.com>
2023-11-16 21:27:38 -0500

    fixed tons of bugs, semi functional now

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
-
 const freelancers = [
-    { name: "Jenny S.", price: 180, occupation: "programmer" },
-    { name: "Peter M.", price: 135, occupation: "programmer" },
-    { name: "Dr. Funke", price: 105, occupation: "Therapist" },
-    { name: "Alice P.", price: 65, occupation: "teacher" },
-    { name: "Charles Twitt esq.", price: 99, occupation: "Lawyer" },
-    { name: "Abe Froman", price: 250, occupation: "Sausage King" },
-    { name: "Ernie McCracken", price: 200, occupation: "Life Coach" },
-    { name: "Luigi", price: 55, occupation: "Plumber" },
-  ];
+  { name: "Jenny S.", occupation: "programmer", price: 180 },
+  { name: "Peter M.", occupation: "programmer", price: 135 },
+  { name: "Dr. Funke", occupation: "Therapist", price: 105 },
+  { name: "Alice P.", occupation: "teacher", price: 65},
+  { name: "Charles Twitt esq.", occupation: "Lawyer", price: 99 },
+  { name: "Abe Froman", occupation: "Sausage King", price: 250 },
+  { name: "Ernie McCracken", occupation: "Life Coach", price: 200 },
+  { name: "Luigi", occupation: "Plumber", price: 55 },
+];
+const maxLength = 8;
 const availableFreelancers = [];
-const addFreelancerIntervalId = setInterval(addFreelancer, 3000);
-
-display();
-
-const display = () => {
-    const flContainer = document.querySelector("#freelist");
-    const freelancerElements = freelancers.map((freelancer) => {
-      const newFreelancer = document.createElement("li");
-      newFreelancer.classList.add(freelancer.name, freelancer.price, freelancer.occupation);
-      return newFreelancer;
-    });
-    flContainer.replaceChildren(...freelancerElements);
-}
 
 const addFreelancer = () => {
-    const freelancer = freelancers[Math.floor(Math.random() * freelancers.length)];
-    availableFreelancers.push({freelancer})
-    display();
-}
+  if(availableFreelancers.length > maxLength){
+    return;
+  }
+  const freelancer = freelancers[Math.floor(Math.random() * freelancers.length)];
+  availableFreelancers.push(freelancer);
+  console.log(availableFreelancers);
+};
+
+const render = () => {
+  const table = document.querySelector("#freelance_table");
+  const freelancerElements = availableFreelancers.forEach((freelancer) => {
+    const row = document.createElement("tr");
+    // const newFreelancer = document.createElement("td");
+    // newFreelancer.textContent = freelancer.name;
+    for (const key in freelancer) {
+        if (key in freelancer){
+            const newFreelancer = document.createElement("td");
+            newFreelancer.textContent = freelancer[key];
+            row.appendChild(newFreelancer);
+        }
+    }
+    console.log("name", freelancer.name);
+    table.appendChild(row);
+    // newFreelancer.textContent = freelancer.name;
+  });
+
+  // console.log(freelancerElements);
+  //   flContainer.replaceChildren(...freelancerElements);
+};
+
+// console.log(availableFreelancers);
+const addFreelancerIntervalId = setInterval(addFreelancer, 3000);
+const addRenderIntervalId = setInterval(render, 10000);
+
+const updateAverage = () => {};
 addFreelancer();
+render();
